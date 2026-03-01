8b810593978ac0acd8c1818f8f6efb9ddb367124
Neeraj <neerajnanwal17@gmail.com>
2026-03-01 14:30:05 +0530

    hogya

---
+++ 03_conversionoperation.js
@@ -0,0 +1,41 @@
+let age = "33" 
+
+console.log(typeof age);
+console.log(typeof(age));
+let val = "33abc"
+//convert stringto number 
+let value = Number(age)
+let val1 = Number(val)
+
+console.log(typeof(value))
+console.log(typeof(val))
+
+// let score = null 
+// let score1 = Number(score)
+// console.log(score1)
+
+// let score = undefined
+// let score1 = Number(score)
+// console.log(score1)
+
+let score = true 
+let score1 = Number(score)
+console.log(score1)
+
+// "33" = 33 
+// "33abc" = nan 
+// true = 1 
+// false = 0 
+
+
+let isloogedin = "neeraj"
+//convert the number to bollean format in the form of true of false 
+let bolleanisloogedin = Boolean(isloogedin)
+
+console.log(bolleanisloogedin);
+
+// 1 = true 
+// 0 = false 
+// "" = false 
+// "neeraj" = true 
+
---
+++ 04_opertions.js
@@ -0,0 +1,52 @@
+let value = 3 
+let negvalue = -value //convert to negative value 
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2); //multiply 
+// console.log(2**2); //power 
+// console.log(2/2); // diide 
+// console.log(2%3); //remmiander 
+
+let str = "hello"
+let str2 = " neeraj"
+let str3 = str + str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1+ 1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2+ 2);
+
+// console.log(true);
+
+// console.log(+true); //gave 1 as output 
+// // console.log(ture+); //gave error 
+// console.log(+""); // it gave 0 
+
+
+// ********comparison *********
+
+// console.log(2 > 3);
+// console.log(2 >=3);
+// console.log(2 <3);
+// console.log(2 <= 3);
+// console.log(2 == 3);
+// console.log(2 != 3);
+// console.log(2 > 3);
+
+// console.log("2" > 1 ); // it auto convert string to number and then compare to the string 
+// console.log("02" > 1 );
+
+// console.log(null > 0 );
+// console.log(null ==0 );
+// console.log(null >= 0 );
+// console.log(null <= 0 );
+// console.log(null < 0 );
+
+
+console.log("2" === 2 ) // ===equal check thier data types also 
+
+console.log("2" == 2 ) // it only convert the type and perform conparison 
