0cb09ca18f803743955ecb4a546c20aecf2af3b1
Neeraj <neerajnanwal17@gmail.com>
2026-03-01 13:32:19 +0530

    datatype

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountid = 121212  //now the value be nnever changed once decared with const 
+let accountemail = "neerajnanwal17@gmail.com" 
+var accountpassword = 123456
+account_city = "Jaipur "
+
+// accountid = 2 
+accountemail = "neeraj12"
+accountpassword = 155155
+account_city = "rohtak "
+console.log(accountid); // it show only th 121212 not 2 
+console.table([accountid,accountemail,accountpassword,account_city])
+// it means the value cannot be changed into the const and let var me value chnage ho jayegi 
+//  we cannot use this var anymore beacus of issue in block oriented 
---
+++ o2_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version 
+
+// alert("hello")  we are using the nodejs not browser 
+// alert create a popup on the browser page 
+
+let name = "neeaj"
+let age = 18 
+let isloggedin = false 
+
+// premiitive data type 
+// number => 2^53 
+//bigint => 
+// string => single quote or dobule 
+// boolean => true false 
+//null => standalone value 
+//undefined => variable is declared but no value is assigned 
+// symbol => for uniqueness we use symbol 
+
+//object 
+console.log(typeof age )
+console.log(typeof null);
+console.log(typeof name);
+
